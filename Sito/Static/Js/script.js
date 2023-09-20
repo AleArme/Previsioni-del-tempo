c3b11bc0b51cd79d23112b14618213d9c89860ae
AleArme <>
2023-09-20 13:41:56 +0200

    Previsione2

--- Sito/Static/Js/script.js
+++ Sito/Static/Js/script.js
@@ -7,21 +7,29 @@ navigator.geolocation.getCurrentPosition(
         console.log(event)
         latitudine = event.coords.latitude
         longitudine = event.coords.longitude
-        createMap()
+        createMap(latitudine, longitudine)
     },
 
     function (event){
         console.log("L'utente non ha accettato")
         console.log(event)
-        createMap()
+        createMap(latitudine, longitudine)
     }
 )
 
+        function createMap(latitudine, longitudine){
+            let map = L.map('map').setView([latitudine, longitudine], 19);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,     
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-            function createMap(){
-                let map = L.map('map').setView([latitudine, longitudine], 10);
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,     
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
-            }
+            let marker = L.marker([latitudine, longitudine]).addTo(map);
+
+            map.on("click", function (event){
+                console.log(event)
+
+                marker.remove()
+                marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map)
+            })
+        }
